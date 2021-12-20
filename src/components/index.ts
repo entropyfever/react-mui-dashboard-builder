d6d1ca24445d6ee9c07612656554b2b7381f2ce1
entropyfever <xmiamis95@gmail.com>
2021-12-20 18:27:37 +0200

    fix: hope so

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,13 +1,9 @@
 // make sure you import all components into this file
-
 export * from './Example';
 export * from './Dummy';
 export * from './DashboardBuilderProvider';
 export * from './SortableTree';
-
 export * from './DashboardPreview'
-
 export * from './TreeNode'
-
 export * from './StyleEditor'
 
